175a727d8f4307e7a87b7b0cd2b00ef0133af2c3
twosson <zebedaios.li@gmail.com>
2023-10-22 21:25:51 +0800

    add tag

--- demo/main.go
+++ demo/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	var index *int
-	s, err := balancer.RoundRobin(fmt.Sprintf("%s:%d", "10.0.8.10", 8500), index, "file-server", "")
+	var index int
+	s, err := balancer.RoundRobin(fmt.Sprintf("%s:%d", "10.0.8.10", 8500), &index, "file-server", "")
 	if err != nil {
 		log.Log.Errorf("Discovery service error: %v", err)
 		return
